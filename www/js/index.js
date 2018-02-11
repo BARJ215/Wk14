5ffade09597152cfb2146a7a9024f06637564925
BARJ215 <32267193+BARJ215@users.noreply.github.com>
2018-02-11 19:07:21 +0000

    DataBinding

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,7 @@ Backendless.initApp("1F116359-9934-2652-FF41-EC23042C0400","B59AA48F-500F-B1E8-F
 
 $(document).on("pageshow","#pageone",onPageShow);
 $(document).on("click","#addTaskButton",onAddTask);
+$(document).on("click","#removeTaskButton",onDeleteTask);
 
 function onPageShow(){
     console.log("page shown");
@@ -12,9 +13,11 @@ function processResults(tasks){
     //display the first task in an array of tasks/
     //alert(tasks[0].Task);
     $('#taskList').empty();
+    $('#removeTaskSelect').empty();
     //add each new tasks
     for(var i=0; i<tasks.length;i++){
-        $('#taskList').append("<li>"+tasks[i].Task+"</li>");        
+        $('#taskList').append("<li>"+tasks[i].Task+"</li>");
+        $('#removeTaskSelect').append("<option value="+tasks[i].objectId+">"+tasks[i].Task+"</option>");
     }
     //refresh the listview
     $('#taskList').listview('refresh');
@@ -28,11 +31,25 @@ function onAddTask(){
     var newTask={};
     newTask.Task=taskText;
     //Save new object
-    Backendless.Data.of("Tasks").save(newTask).then(saved).catch(error);   
+    Backendless.Data.of("Tasks").save(newTask).then(saved).catch(error);
+    
+}
+
+function onDeleteTask(){
+    console.log("delete task button clicked");
+    var selectID =$( "#removeTaskSelect option:selected" ).val();
+    //Remove Object by ID
+    Backendless.Data.of("Tasks").remove(selectID).then(removed).catch(error);
 }
 
 function saved(savedTask){
-    console.log("new Contact instance has been saved" + savedTask);
+    console.log("new object instance has been saved" + savedTask);
+    onPageShow();
+}
+
+function removed(removedTask){
+    console.log("object instance has been removed" + removedTask);
+    onPageShow();
 }
     
 function error(err){
