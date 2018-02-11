4c78cda3549bf5560546b0f1bf4d39b308fb6d8c
BARJ215 <32267193+BARJ215@users.noreply.github.com>
2018-02-11 17:20:43 +0000

    a

---
+++ www/js/index.js
@@ -0,0 +1,21 @@
+Backendless.initApp("1F116359-9934-2652-FF41-EC23042C0400","B59AA48F-500F-B1E8-FF7B-EACAB3399500");
+
+$document.on("pageshow","#pageone",onPageShow);
+
+function onPageShow(){
+    console.log("page shown");
+    
+    Backendless.Data.of("TASKS").find().then(processResults).catch(error);
+    
+    function processResults(tasks){
+        //display the first task in an array of tasks/
+        alert(tasks[0].Task);
+            
+    }
+    
+    function error(err){
+        alert(err);
+        
+    }
+    
+}
