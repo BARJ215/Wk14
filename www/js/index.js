4db1ccae6bb38007c3c32c0c1546907d925ae286
BARJ215 <32267193+BARJ215@users.noreply.github.com>
2018-02-11 17:34:09 +0000

    f

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,6 @@
 Backendless.initApp("1F116359-9934-2652-FF41-EC23042C0400","B59AA48F-500F-B1E8-FF7B-EACAB3399500");
 
-$document.on("pageshow","#pageone",onPageShow);
+$(document).on("pageshow","#pageone",onPageShow);
 
 function onPageShow(){
     console.log("page shown");
